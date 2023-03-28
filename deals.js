70a0b8ef0351eb22888b388c2140db2511de632c
gmartini2019 <95395167+gmartini2019@users.noreply.github.com>
2023-03-28 19:56:39 -0400

    Add files via upload

---
+++ deals.js
@@ -0,0 +1,20 @@
+const grid = `div class = 'fdeals bg-purple-200 fdeals'>
+<img src = 'https://www.samovartea.com/wp-content/uploads/2019/10/Chai-Mug-Side-View-1.jpg' class="h-200 w-100" alt = 'image'> <h3>Chirstmas mug!!</h3><br> 25% Discount
+</div>
+<div  class = 'fdeals bg-purple-200 fdeals'>
+<img src = 'https://cdn.media.amplience.net/i/partycity/P899513?$large$&fmt=auto&qlt=default' class="h-200 w-100"  alt = 'image'><h3>Christmas sweater!!</h3><br>23% Discount
+</div>
+<div class = 'fdeals bg-purple-200 fdeals'>
+<img src = 'https://static.displate.com/857x1200/displate/2019-12-07/3cc48cd3cc84e74145caba17f60d6085_7083bfa02f4391d188680a1f32338601.jpg' class="h-200 w-100"  alt = 'image'><h3>Christmas Cat!!</h3> <br>23% Discount
+
+</div>
+<div class = 'fdeals bg-purple-200 fdeals'>
+    <img src = 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-13-pink-select-2021?wid=940&hei=1112&fmt=png-alpha&.v=1645572315935' class="h-200 w-100"  alt = 'image'><h3>Iphone!!</h3> <br>20% Discount
+
+</div>
+<div  class = 'fdeals bg-purple-200'>
+<img src = 'http://cdn.shopify.com/s/files/1/0130/8226/3616/products/KOC2021TreesVertical-1.jpg?v=1663977558' class="h-200 w-100"  alt = 'image' ><h3>Christmas Tree</h3><br>20% Discount
+
+</div>`;
+
+var model = document.getElementById('deals').innerHTML = grid;
